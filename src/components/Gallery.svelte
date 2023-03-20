ee58f301904c727bd75c2374ab07e750f82645bb
timmedia <contact@tim-media.com>
2023-03-20 10:30:58 +0100

    feat: UI improvements

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -14,8 +14,36 @@
   ];
 </script>
 
-<Carousel autoplay autoplayDuration={5000}>
-  {#each images as image}
-    <img src={image} alt="" />
-  {/each}
-</Carousel>
+<div class="rounded-sm overflow-hidden relative">
+  <Carousel
+    autoplay
+    autoplayDuration={5000}
+    arrows={true}
+    dots={false}
+    let:showPrevPage
+    let:showNextPage
+  >
+    <div
+      slot="prev"
+      on:click={showPrevPage}
+      on:keypress={showPrevPage}
+      class="absolute h-full w-16 text-white font-black text-4xl bg-gradient-to-l to-[rgba(0,0,0,0.3)] from-transparent left-0 flex flex-col justify-center items-center cursor-pointer z-10 select-none"
+    >
+      ⟨
+    </div>
+    <div
+      slot="next"
+      on:click={showNextPage}
+      on:keypress={showNextPage}
+      class="absolute h-full w-16 text-white font-black text-4xl bg-gradient-to-l from-[rgba(0,0,0,0.3)] to-transparent right-0 flex flex-col justify-center items-center cursor-pointer z-10 select-none"
+    >
+      ⟩
+    </div>
+
+    {#each images as image}
+      <div class="w-full max-h-[650px]">
+        <img class="select-none" src={image} alt="" />
+      </div>
+    {/each}
+  </Carousel>
+</div>
