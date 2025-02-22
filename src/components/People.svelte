8e24d10f44520e3081899a3d53f873ee0dad419a
timmedia <contact@tim-media.com>
2025-02-22 16:00:50 +0100

    feat: update people

--- src/components/People.svelte
+++ src/components/People.svelte
@@ -2,17 +2,17 @@
   type Person = {
     name: string;
     role?: string;
-    position: string;
-    phone: string;
-    mail: string;
-    picture: string;
+    position?: string;
+    phone?: string;
+    mail?: string;
+    picture?: string;
   };
   const people: Person[] = [
     {
       name: "Karin Klemm",
-      role: "Hospizseelsorgerin",
+      role: "Seelsorgerin",
       position: "Präsidentin",
-      phone: "056 470 35 12",
+      // phone: "056 470 35 12",
       mail: "imfluss@karinklemm.ch",
       picture: "/karin.jpg",
     },
@@ -27,15 +27,11 @@
       name: "Martin Pirktl",
       position: "Musiker",
       phone: "079 321 18 63",
-      mail: "pirktl@bluewin.ch",
+      mail: "martin.pirktl@gmail.com",
       picture: "/martin.jpg",
     },
     {
-      name: "Daniela Mustone",
-      position: "Krebsliga Aargau ",
-      phone: "079 716 42 87",
-      mail: "dani.mustone@gmail.com",
-      picture: "/daniela.jpg",
+      name: "Margareta Graf",
     },
     {
       name: "Peter Wendel",
@@ -53,20 +49,32 @@
       class="flex flex-row-reverse sm:flex-row justify-between sm:justify-start"
     >
       <div
-        class="relative overflow-hidden h-32 w-24 md:w-32 md:h-40 lg:w-48 lg:h-64 sm:mr-3 shadow-lg"
+        class="relative overflow-hidden h-32 w-24 md:w-32 md:h-40 lg:w-48 lg:h-64 sm:mr-3 shadow-lg bg-slate-400"
       >
-        <img
-          class="absolute h-full w-full object-cover rounded-sm"
-          src={person.picture}
-          alt={person.name}
-        />
+        {#if person?.picture}
+          <img
+            class="absolute h-full w-full object-cover rounded-sm"
+            src={person.picture}
+            alt={person.name}
+          />
+        {/if}
       </div>
       <div class="flex-shrink">
         <h1 class="text-2xl">{person.name}</h1>
-        <p class="mb-0">{person?.role ?? ""}</p>
-        <p class="mb-1 md:mb-2">{person.position}</p>
-        <a class="block md:mb-1" href={`tel:${person.phone}`}>{person.phone}</a>
-        <a class="block" href={`mailto:${person.mail}`}>{person.mail}</a>
+        {#if person?.role}
+          <p class="mb-0">{person.role}</p>
+        {/if}
+        {#if person?.position}
+          <p class="mb-1 md:mb-2">{person.position}</p>
+        {/if}
+        {#if person?.phone}
+          <a class="block md:mb-1" href={`tel:${person.phone}`}
+            >{person.phone}</a
+          >
+        {/if}
+        {#if person?.mail}
+          <a class="block" href={`mailto:${person.mail}`}>{person.mail}</a>
+        {/if}
       </div>
     </section>
   {/each}
